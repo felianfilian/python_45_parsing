e13d53e748d7fb288621dadddc778a012e1e2b2a
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-13 00:42:09 +0100

    import beautiful soup

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 
+from bs4 import BeautifulSoup
+
 try:
     with open("website.html") as file:
         content = file.read()
         print(content)
 except FileNotFoundError:
     print("File not found")
+
+soup = BeautifulSoup()
+
+
+
 
