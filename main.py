4a347ce076285abde2976a7688bebf0c1b23caec
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-15 00:44:04 +0100

    get title

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import requests
 response = requests.get("https://news.ycombinator.com/news")
 web_page = response.text
 soup = BeautifulSoup(web_page, "html.parser")
+
+print(soup.title)
+
+# article_tag = soup.find(name="a", class_="storylink")
+# article_text = article_tag.getText()
+# print(article_text)
+
