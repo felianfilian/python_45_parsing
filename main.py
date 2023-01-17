6d89e9c8b74cea5e4a10325f23a2c70963f3d4cc
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-17 22:49:53 +0100

    fetch website html

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+print(website_html)
+
